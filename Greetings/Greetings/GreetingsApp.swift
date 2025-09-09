00d493a41f6636e365d16fa9af7238fd68ff942d
KonarAndriiana <andriianakonar@gmail.com>
2025-09-09 21:42:16 +0200

    change GreetingsApp to adapt different language layout

--- Greetings/Greetings/GreetingsApp.swift
+++ Greetings/Greetings/GreetingsApp.swift
@@ -14,11 +14,17 @@ import SwiftUI
 struct GreetingsApp: App {
     
     @State private var language : String = "en"
-    @State private var languageDirection : String = LEFT_TO_RIGHT
+    @State private var languageDirectionString : String = LEFT_TO_RIGHT
+    
+    var layoutDirection : LayoutDirection {
+        languageDirectionString == LEFT_TO_RIGHT ? .leftToRight : .rightToLeft
+    }
     
     var body: some Scene {
         WindowGroup {
-            MainView(language: $language, layoutDirectionString: $languageDirection)
+            MainView(language: $language, layoutDirectionString: $languageDirectionString)
+                .environment(\.locale, Locale(identifier: language))
+                .environment(\.layoutDirection, layoutDirection)
         }
     }
 }
