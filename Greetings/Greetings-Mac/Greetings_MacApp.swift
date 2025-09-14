0a60569a4b5052da4146f1c45171eb5aa82545df
KonarAndriiana <andriianakonar@gmail.com>
2025-09-14 10:11:00 +0200

    modify structure for shared and targets group

---
+++ Greetings/Greetings-Mac/Greetings_MacApp.swift
@@ -0,0 +1,17 @@
+//
+//  Greetings_MacApp.swift
+//  Greetings-Mac
+//
+//  Created by Andriiana Konar on 12/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct Greetings_MacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
